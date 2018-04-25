32d6dac99011783110df06c47485a65744138155
dasdzrl <986396216@qq.com>
2018-04-25 11:41:33 +0800

    新增我的页面

--- NSMall/Publish(发布)/NSPublishViewController.m
+++ NSMall/Publish(发布)/NSPublishViewController.m
@@ -9,7 +9,15 @@
 #import "NSPublishViewController.h"
 
 @interface NSPublishViewController ()
+/** 线上商品 按钮 */
+@property(nonatomic,strong)UIButton * onLineGoods;
+/** 线下店铺 按钮 */
+@property(nonatomic,strong)UIButton * underLineShop;
 
+/** 线上 */
+@property(nonatomic,strong)UILabel *onLine;
+/** 线下 */
+@property(nonatomic,strong)UILabel *underLine;
 @end
 
 @implementation NSPublishViewController
@@ -17,6 +25,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self createUI];
+}
+
+-(void)createUI{
+    self.onLineGoods  = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.onLineGoods addTarget:self action:@selector(getGoodsOrShop:) forControlEvents:UIControlEventTouchUpInside];
+    self.onLineGoods.left = GetScaleWidth(65);
+    self.onLineGoods.top = GetScaleWidth(425);
+    self.onLineGoods.size = CGSizeMake(GetScaleWidth(80), GetScaleWidth(80));
+    self.onLineGoods.tag = 10;
+    [self.onLineGoods setImage:[UIImage imageNamed:@"publish_ico_goods"] forState:UIControlStateNormal];
+    [self.view addSubview:self.onLineGoods];
+    
+    self.onLine = [[UILabel alloc] init];
+    self.onLine.centerX = self.onLineGoods.centerX-GetScaleWidth(27);
+    self.onLine.y = CGRectGetMaxY(self.onLineGoods.frame)+GetScaleWidth(15);
+    self.onLine.font = [UIFont systemFontOfSize:kFontNum15];
+    self.onLine.textColor = KColorText323232;
+    self.onLine.text = KLocalizableStr(@"线上商品");
+    [self.onLine sizeToFit];
+    [self.view addSubview:self.onLine];
+    
+    self.underLineShop  = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.underLineShop addTarget:self action:@selector(getGoodsOrShop:) forControlEvents:UIControlEventTouchUpInside];
+    self.underLineShop.right = GetScaleWidth(246);
+    self.underLineShop.top = GetScaleWidth(425);
+    self.underLineShop.size = CGSizeMake(GetScaleWidth(80), GetScaleWidth(80));
+    self.underLineShop.tag = 20;
+    [self.underLineShop setImage:[UIImage imageNamed:@"publish_ico_shop"] forState:UIControlStateNormal];
+    [self.view addSubview:self.underLineShop];
+    
+    self.underLine = [[UILabel alloc] init];
+    self.underLine.centerX = self.underLineShop.centerX-GetScaleWidth(27);
+    self.underLine.y = CGRectGetMaxY(self.underLineShop.frame)+GetScaleWidth(15);
+    self.underLine.font = [UIFont systemFontOfSize:kFontNum15];
+    self.underLine.textColor = KColorText323232;
+    self.underLine.text = KLocalizableStr(@"线下店铺");
+    [self.underLine sizeToFit];
+    [self.view addSubview:self.underLine];
+}
+
+-(void)getGoodsOrShop:(UIButton *)button{
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +75,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
