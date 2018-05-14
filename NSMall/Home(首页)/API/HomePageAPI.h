2d1484795bc0976607a073f945bad1945adb559c
dasdzrl <986396216@qq.com>
2018-05-14 09:31:53 +0800

    514保存代码

--- NSMall/Home(首页)/API/HomePageAPI.h
+++ NSMall/Home(首页)/API/HomePageAPI.h
@@ -11,8 +11,6 @@
 #import "AdvertListModel.h"
 #import "CategoryModel.h"
 #import "CategoryListModel.h"
-#import "SearchModel.h"
-#import "SearchParam.h"
 
 
 @interface HomePageAPI : NSObject
@@ -33,21 +31,10 @@
     获取分类信息接口
 */
 + (void)getProductCategoryList:(nullable NSString *)parentId success:(void(^)(CategoryListModel * _Nullable result))success failure:(void (^)(NSError *error))failure;
-<<<<<<< HEAD
-=======
-
->>>>>>> eaeadc9268cf35ed49897375bc350de54da4ac27
 
 /*
     改变商品的点赞状态
  */
 + (void)changeProductLikeState:(NSString *)productId success:(void (^)(NSInteger likeNumber))success failure:(void (^)(NSError *error))failure;
-
-
-/*
-    搜索商铺或者商品
- */
-+ (void)searchProductOrShop:(nullable id)param success:(void (^)(SearchModel *result))success failure:(void (^)(NSError *error))failure;
-
 
 @end
