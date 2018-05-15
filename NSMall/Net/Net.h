927c8f5a0bd13a8290e55bf31bb61081c77dbf66
yangxinda <undeadableheart@gmail.com>
2018-05-15 09:25:15 +0800

    更新文件上传接口

--- NSMall/Net/Net.h
+++ NSMall/Net/Net.h
@@ -22,6 +22,7 @@ extern AFHTTPSessionManager *httpManager;
 + (void)requestWithPost:(nullable id)params function:(nullable NSString *)function showHUD:(nullable NSString *)showHUD resultClass:(nullable Class)resultClass success:(nullable void (^)(id _Nullable resultObj))success failure:(nullable void (^)(NSError * _Nullable error))failure;
 + (void)requestWithGet:(nullable id)params function:(nullable NSString *)function showHUD:(nullable NSString *)showHUD resultClass:(nullable Class)resultClass success:(nullable void (^)(id _Nullable resultObj))success failure:(nullable void (^)(NSError * _Nullable error))failure;
 
-
+//上传文件
++ (void)uploadDataWithPost:(NSData *)data function:(NSString *)function success:(void (^)(NSDictionary *result))success failure:(void (^)(NSError *error))failure;
 
 @end
